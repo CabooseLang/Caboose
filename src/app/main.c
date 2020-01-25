7203c52e79878137163663f7151c14bd6201a348
Landon Gravat <railinator4903@gmail.com>
2020-01-24 21:52:24 -0600

    everything done except for classes, closures, and garbage collection

--- src/app/main.c
+++ src/app/main.c
@@ -4,38 +4,43 @@
 #include <stdlib.h>
 
 static void repl() {
-	char line[1024];
-	for (;;) {
-	  	printf("[>] ");
+  printf("Caboose Prompt\n");
+  char line[1024];
+  for (;;) {
+	printf("[>] ");
 
-	  	if (!fgets(line, sizeof(line), stdin)) {
-			printf("\n");
-			break;
-	  	}
-
-	  	interpret(line);
-	}
-}
-
-static void runFile(const char* path) {
-	char* source = readFile(path);
-	InterpretResult result = interpret(source);
-	free(source);
-
-	if (result == INTERPRET_COMPILE_ERROR) exit(65);
-	if (result == INTERPRET_RUNTIME_ERROR) exit(70);
-}
-
-int main(int argc, const char** argv) {
-	initVM();
-
-	if (argc == 1) repl();
-	else if (argc == 2) runFile(argv[1]);
-	else {
-	  	fprintf(stderr, "Usage: cb [path]\n");
-	  	exit(64);
+	if (!fgets(line, sizeof(line), stdin)) {
+	  printf("\n");
+	  break;
 	}
 
-	freeVM();
-	return 0;
+	interpret(line);
+  }
+}
+
+static void runFile(const char *path) {
+  char *source = readFile(path);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR)
+	exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR)
+	exit(70);
+}
+
+int main(int argc, const char **argv) {
+  initVM();
+
+  if (argc == 1)
+	repl();
+  else if (argc == 2)
+	runFile(argv[1]);
+  else {
+	fprintf(stderr, "Usage: cb [path]\n");
+	exit(64);
+  }
+
+  freeVM();
+  return 0;
 }
