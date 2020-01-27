df9fb6865c82c034439719f43655258796527ee3
Landon Gravat <railinator4903@gmail.com>
2020-01-26 22:52:56 -0600

    bool and len natives

--- src/natives.c
+++ src/natives.c
@@ -1,8 +1,11 @@
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "natives.h"
 #include "object.h"
+#include "util.h"
 #include "vm.h"
 
 static Value
@@ -10,14 +13,68 @@ clockNative(int argCount, Value* args) {
     return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
 }
 
+static Value
+timeNative(int argCount, Value* args) {
+    return NUMBER_VAL((double)time(NULL));
+}
+
+static Value
+strNative(int argCount, Value* args) {
+    if (argCount != 1) {
+        runtimeError("str() takes exactly 1 argument (%d given).", argCount);
+        return NIL_VAL;
+    }
+
+    if (!IS_STRING(args[0])) {
+        char *valueString = valueToString(args[0]);
+
+        ObjString *string = copyString(valueString, strlen(valueString));
+        free(valueString);
+
+        return OBJ_VAL(string);
+    }
+
+    return args[0];
+}
+
+static Value
+boolNative(int argCount, Value* args) {
+    if (argCount != 1) {
+        runtimeError("bool() takes exactly 1 argument (%d given).", argCount);
+        return NIL_VAL;
+    }
+
+    return BOOL_VAL(!isFalsey(args[0]));
+}
+
+static Value
+lenNative(int argCount, Value* args) {
+    if (argCount != 1) {
+        runtimeError("bool() takes exactly 1 argument (%d given).", argCount);
+        return NIL_VAL;
+    }
+
+    if (IS_STRING(args[0])) return NUMBER_VAL(AS_STRING(args[0])->length);
+
+    runtimeError("Unsupported type passed to len()");
+    return NIL_VAL;
+}
+
 const char* nativeNames[] = {
-  "clock",
+    "clock",
+    "time",
+    "str",
+    "bool",
+    "len",
 };
 
 NativeFn nativeFunctions[] = {
-  clockNative,
+    clockNative,
+    timeNative,
+    strNative,
+    boolNative,
+    lenNative,
 };
-
 
 static bool
 printNative(int argCount, Value* args) {
@@ -36,19 +93,16 @@ printNative(int argCount, Value* args) {
 }
 
 const char* nativeVoidNames[] = {
-  "print",
+    "print",
 };
 
 NativeFnVoid nativeVoidFunctions[] = {
-  printNative,
+    printNative,
 };
 
-void defineAllNatives() {
-    for (uint8_t i = 0; i < sizeof(nativeNames) / sizeof(nativeNames[0]); ++i) {
-        defineNative(nativeNames[i], nativeFunctions[i]);
-    }
+void
+defineAllNatives() {
+    for (uint8_t i = 0; i < sizeof(nativeNames) / sizeof(nativeNames[0]); ++i) defineNative(nativeNames[i], nativeFunctions[i]);
 
-    for (uint8_t i = 0; i < sizeof(nativeVoidNames) / sizeof(nativeVoidNames[0]); ++i) {
-        defineNativeVoid(nativeVoidNames[i], nativeVoidFunctions[i]);
-    }
+    for (uint8_t i = 0; i < sizeof(nativeVoidNames) / sizeof(nativeVoidNames[0]); ++i) defineNativeVoid(nativeVoidNames[i], nativeVoidFunctions[i]);
 }
